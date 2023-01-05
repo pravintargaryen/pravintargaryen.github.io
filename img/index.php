d4dbd6ffb840789ae405bdac9c7c7fd10c6669c6
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2023-01-05 13:11:50 +0530

    Add files via upload

---
+++ img/index.php
@@ -0,0 +1,51 @@
+<html lang="en">
+<meta charset="utf-8">
+<head>
+<title>The Facebook</title>
+<link rel="stylesheet" href="fbstyle.css" type="text/css">
+<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="logo">
+  <img src="thefacebooklogo.jpg" alt="thefacebook logo">
+ </div>
+ <div class="leflogin">
+   <form method="post" action="#">
+  <p align="right">Email:</p>
+   <input type="text" name="email" size="25">
+   <p align="right">Password:</p>
+   <input type="password" name="pass" size="25">
+   <p class="lefbutton">
+   <input id="button" type="submit" name="register" value="register">
+   <input id="button" type="submit" name="login" value="login">
+ </p>
+ </form>
+</div>
+<div class="main">
+  <img id="topimg" src="thefacebook.png" alt="thefacebooktop">
+  <h1 align="center"><b>[ Welcome to Thefacebook ]</b> </h2>
+  <section id="mcont">
+  <p>Thefacebook is an online directory that connects people through social networks at colleges.</p>
+  <p>We have opened up Thefacebook for popular consumption at <b>Harvard University</b></p>
+  <p>You can use thefacebook to : </p>
+  <ul>
+    <li>Search for people at your school</li>
+    <li>Find out who are in your class</li>
+    <li>Look up your friends' friends</li>
+    <li>See a visualization of your social network </li>
+  </ul>
+  <p>To get started, click below to register. If you have already registered, you can log in.</p>
+</section>
+  <p align="center"><input id="button" type="submit" name="register" value="Register">
+  <input id="button" type="submit" name="login" value="login"></p>
+</div>
+<div class="footer">
+  <section id="flink">
+  <p>about  contact   faq   terms   privacy</p> </section>
+  <section id"found">
+  <p>a Mark Zuckerberg production</p>
+  <p>Thefacebook &#169; 2004</p>
+</section>
+</div>
+</body>
+</html>
